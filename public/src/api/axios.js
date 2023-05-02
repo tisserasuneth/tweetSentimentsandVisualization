31083ef367c957e57e749356aa40b655a258dc68
tisserasuneth@gmail.com <tisserasuneth@gmail.com>
2023-05-02 01:35:34 -0400

    updates

--- public/src/api/axios.js
+++ public/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://thebrewanalysis-frontend.onrender.com'
+    baseURL: 'https://thebrewanalysis-backend.onrender.com'
 });
