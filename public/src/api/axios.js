0c8d5717cffad78016276e47bbcc8e68175b680f
tisserasuneth@gmail.com <tisserasuneth@gmail.com>
2023-05-02 14:33:07 -0400

    test

--- public/src/api/axios.js
+++ public/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://tweet-sentimentsand-visualization-t3aa.vercel.app'
+    baseURL: 'https://thebrewanalysis-backend.onrender.com'
 });
