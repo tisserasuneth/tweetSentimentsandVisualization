1953dc5d1f6db5022764a43b69b55f90a98d6c91
tisserasuneth@gmail.com <tisserasuneth@gmail.com>
2023-05-02 14:29:28 -0400

    test

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ const cors = require('cors');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
+    if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
     res.setHeader["Access-Control-Allow-Origin","*"]
     res.setHeader["Access-Control-Allow-Methods","*"]
     res.setHeader["Access-Control-Allow-Headers","*"]
