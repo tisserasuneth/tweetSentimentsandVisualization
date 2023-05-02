5d8b1b19e4a9d1a049b2fe5db5f85b9431b49854
tisserasuneth@gmail.com <tisserasuneth@gmail.com>
2023-05-01 23:12:59 -0400

    test

--- server/sentiment.py
+++ server/sentiment.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# from transformers import pipeline
+from transformers import pipeline
 import sys
 import json
 
@@ -17,31 +17,31 @@ n = 50
 
 def analyze(CSVName):
     print('hello')
-    # df = pd.read_csv(CSVName)
-    # tweets = df.head(n)['content'].values
-    # sentiment_values = []
-    # for tweet in tweets:
-    #     tweet_words = []
-    #     for word in tweet.split(' '):
-    #         if word.startswith('@') and len(word) > 1:
-    #             word = '@user'
-    #         elif word.startswith('http'):
-    #             word = "http"
-    #         tweet_words.append(word)
+    df = pd.read_csv(CSVName)
+    tweets = df.head(n)['content'].values
+    sentiment_values = []
+    for tweet in tweets:
+        tweet_words = []
+        for word in tweet.split(' '):
+            if word.startswith('@') and len(word) > 1:
+                word = '@user'
+            elif word.startswith('http'):
+                word = "http"
+            tweet_words.append(word)
 
-    #     tweet_proc = " ".join(tweet_words)
+        tweet_proc = " ".join(tweet_words)
 
-    #     model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-    #     revision = "af0f99b"
+        model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+        revision = "af0f99b"
 
-    #     sentiment = pipeline("sentiment-analysis", model=model_name, revision=revision)
-    #     result = sentiment(tweet_proc)[0]
-    #     count(result['label'])
-    #     sentiment_values.append(result['label'])
+        sentiment = pipeline("sentiment-analysis", model=model_name, revision=revision)
+        result = sentiment(tweet_proc)[0]
+        count(result['label'])
+        sentiment_values.append(result['label'])
 
     newDf = pd.read_csv(CSVName,nrows=n)   
-    # newDf.loc[:n-1, 'sentiment'] = sentiment_values
-    # newDf = newDf.loc[newDf['sentiment']=='NEGATIVE']
+    newDf.loc[:n-1, 'sentiment'] = sentiment_values
+    newDf = newDf.loc[newDf['sentiment']=='NEGATIVE']
     labelsAndLoc['latitude'] = (newDf['latitude'].values).tolist()
     labelsAndLoc['longitude'] = (newDf['longitude'].values).tolist()
     jsonFile = json.dumps(labelsAndLoc)
